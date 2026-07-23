b75cfbc52c06caeec1d6d97f6b2a79d4730ed620
galatas <galatasmutz@gmail.com>
2026-07-23 17:53:41 -0300

    feat: atualizar ícone (favicon) para a logo

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -114,7 +114,7 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
         rel: "stylesheet",
         href: appCss,
       },
-      { rel: "icon", href: "/favicon.ico", type: "image/x-icon" },
+      { rel: "icon", href: "/plasmaster-logo-hero.png", type: "image/png" },
     ],
   }),
   shellComponent: RootShell,
